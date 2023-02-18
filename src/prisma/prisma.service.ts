00ffd4082bcdfb2923fdf07514ec609a15d49daa
1zumisawashun <shun.1zumisawa@gmail.com>
2023-02-18 16:14:21 +0900

    fix

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  // prismaClientはnestのモジュールでないのでextends&superで参照する
+  // これでprismaClinetで定義されているdb操作のメソッドを使うことができる
+  constructor(private readonly config: ConfigService) {
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}
