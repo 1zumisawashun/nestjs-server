4d8d837acf621bd226f95728888d5a8bd02bc319
1zumisawashun <shun.1zumisawa@gmail.com>
2023-02-18 22:29:21 +0900

    fix

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,65 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpCode,
+  HttpStatus,
+  Res,
+  Req,
+  Get,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Csrf, Msg } from './interfaces/auth.interface';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Get('/csrf')
+  getCsrfToken(@Req() req: Request): Csrf {
+    return { csrfToken: req.csrfToken() };
+  }
+
+  @Post('signup')
+  signUp(@Body() dto: AuthDto): Promise<Msg> {
+    // req.bodyを取り出すときに@Body()する
+    // 実態はemailとpassのオブジェクト
+    // でビジネスロジックはserviceに任せているので処理はこれだけでok
+    return this.authService.signUp(dto);
+  }
+
+  @HttpCode(HttpStatus.OK) // postはnestの仕様で201になるが「作成する」とloginは意図が違うので200で返すためのデコレータ
+  @Post('login')
+  async login(
+    @Body() dto: AuthDto,
+    @Res({ passthrough: true }) res: Response,
+  ): Promise<Msg> {
+    const jwt = await this.authService.login(dto);
+    // サーバー側でcookieを設定する
+    res.cookie('access_token', jwt.accessToken, {
+      httpOnly: true,
+      secure: false, // 動作確認の時はfalseにする（sameSiteの関係）
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    };
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('/logout')
+  logout(@Req() req: Request, @Res({ passthrough: true }) res: Response): Msg {
+    res.cookie('access_token', '', {
+      httpOnly: true,
+      secure: false, // 動作確認の時はfalseにする（sameSiteの関係）
+      sameSite: 'none',
+      path: '/',
+    });
+    return {
+      message: 'ok',
+    };
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { Request } from 'express';
 import * as cookieParser from 'cookie-parser';
 import * as csurf from 'csurf';
+// csurfは正規のサイトからリクエストがあったか検証する
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true })); // dtoに定義していないフィールドが送られてきたら省いてくれる
   app.enableCors({
     credentials: true, //cookieベースでjwtのやりとりをするのでつける
-    origin: ['http://localhost:3000'],
+    origin: ['http://localhost:3000'], // whitelistにlocalohost3000を登録する
   });
   app.use(cookieParser());
   await app.listen(3005);
